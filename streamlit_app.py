0baabe6e107649723a8c0b2949d87d5c79a3c2a7
Ashli Dougherty <106764633+ashlidougherty@users.noreply.github.com>
2023-01-30 08:43:59 -0600

    Update streamlit_app.py
    added header with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favorites")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-boiled Free-Range Egg")
+streamlit.text("Avocado Toast")
+
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
