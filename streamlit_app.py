2d1ff784b0fe002e28978b99040218a550992d24
Ashli Dougherty <106764633+ashlidougherty@users.noreply.github.com>
2023-01-30 08:45:12 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ streamlit.header("Breakfast Favorites")
 streamlit.text("Omega 3 & Blueberry Oatmeal")
 streamlit.text("Kale, Spinach & Rocket Smoothie")
 streamlit.text("Hard-boiled Free-Range Egg")
-streamlit.text("Avocado Toast")
+streamlit.text("\N{avovado}Avocado Toast")
 
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
